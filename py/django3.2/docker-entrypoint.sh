159f2bc7b6639bd27f05ded0f689dcd37cba493d
Hui <hui.dev@outlook.com>
2021-05-22 10:27:28 +0800

    Update docker-entrypoint.sh

--- py/django3.2/docker-entrypoint.sh
+++ py/django3.2/docker-entrypoint.sh
@@ -2,14 +2,18 @@
 
 APP_ROOT='/usr/src/app'
 STATIC_ROOT='/usr/src/static'
+NODE_MODULES='/usr/src/node_modules'
 
 cd $APP_ROOT
 
 # collect statics
+sed -i '/node_modules/c\"'$NODE_MODULES'",' `find . | grep settings.py`
 sed -i '/^STATIC_ROOT/c\STATIC_ROOT="'$STATIC_ROOT'"' `find . | grep settings.py`
 python manage.py collectstatic --noinput
 
 # remove debug symbol
 sed -i '/^DEBUG/c\DEBUG=False' `find . | grep settings.py`
+
+
 
 exec $@
